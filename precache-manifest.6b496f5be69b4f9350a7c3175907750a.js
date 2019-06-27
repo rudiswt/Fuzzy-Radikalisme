e51bfe1692f1fbc57473be70c35afd85c2c02d08
Rudi Setiawan <rudiswtn@gmail.com>
2019-06-28 00:13:24 +0700

    Updates

---
+++ precache-manifest.6b496f5be69b4f9350a7c3175907750a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d452d5704e89bd252fe83b1c25b85dc8",
+    "url": "/Fuzzy-Radikalisme/index.html"
+  },
+  {
+    "revision": "d04f97887b55a1832dbe",
+    "url": "/Fuzzy-Radikalisme/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "99cdb12b7fc627ecec5b",
+    "url": "/Fuzzy-Radikalisme/static/js/2.823b88c7.chunk.js"
+  },
+  {
+    "revision": "d04f97887b55a1832dbe",
+    "url": "/Fuzzy-Radikalisme/static/js/main.c7b035f1.chunk.js"
+  },
+  {
+    "revision": "7b2875134d48056c885e",
+    "url": "/Fuzzy-Radikalisme/static/js/runtime~main.559b358f.js"
+  }
+]);
